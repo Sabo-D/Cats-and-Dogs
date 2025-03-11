4e1bd4389357e75f3a62cd199040c3fbc1786bd9
Sabo <3186280752@qq.com>
2025-03-11 10:03:00 +0800

    新加ResNet

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import src.ResNet_18 as model
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train_dataloader, val_dataloader = dataset.train_val_dataloader().to(device)
+    train_dataloader, val_dataloader = dataset.train_val_dataloader()
     model = model.ResNet18(model.ResidualBlock).to(device)
     train_process = train_model.model_train(model, train_dataloader, val_dataloader, 10)
     plot_train_process = plot.plot_acc_loss(train_process)
